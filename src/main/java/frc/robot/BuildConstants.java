6f6ee64d6b9c3a667787acaf2e04398e53c70388
reecelikesramen <3149587+reecelikesramen@users.noreply.github.com>
2026-01-12 12:15:43 -0700

    give admin rights to format check, still working on pre-commit hook resilience

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-robot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 9;
-  public static final String GIT_SHA = "ded53ac60071ed7f3ddf3e30185857e1f318dba1";
-  public static final String GIT_DATE = "2026-01-12 18:38:28 GMT";
+  public static final int GIT_REVISION = 10;
+  public static final String GIT_SHA = "ed2984480aa4bbadd20acecf8a5ee934b62c4838";
+  public static final String GIT_DATE = "2026-01-12 18:45:41 GMT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-12 18:44:38 GMT";
-  public static final long BUILD_UNIX_TIME = 1768243478421L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2026-01-12 19:14:17 GMT";
+  public static final long BUILD_UNIX_TIME = 1768245257708L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
