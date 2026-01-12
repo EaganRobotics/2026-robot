92ae1b7d81ea63bea824fad17aabe732bed4913e
reecelikesramen <3149587+reecelikesramen@users.noreply.github.com>
2026-01-12 11:27:56 -0700

    add formatting logic and advantagekit

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2026-robot";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 7;
+  public static final String GIT_SHA = "9c5046bc3510be3467636327cc79d347ba2718db";
+  public static final String GIT_DATE = "2026-01-12 03:53:02 GMT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-01-12 18:23:49 GMT";
+  public static final long BUILD_UNIX_TIME = 1768242229243L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants(){}
+}
