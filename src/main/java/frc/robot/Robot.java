ed2984480aa4bbadd20acecf8a5ee934b62c4838
reecelikesramen <3149587+reecelikesramen@users.noreply.github.com>
2026-01-12 11:45:41 -0700

    Seeing if CI will fix formatting errors manually skipped pre-commit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,7 +14,8 @@ public class Robot extends TimedRobot {
   private final RobotContainer m_robotContainer;
 
   public Robot() {
-    m_robotContainer = new RobotContainer();
+          m_robotContainer =             new RobotContainer();
+      // testing invalid formatting and force commit to see if CI fixes it
   }
 
   @Override
