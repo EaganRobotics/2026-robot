2224f3db95ced6f10c76378c08148438718e51b2
Anvith <819178@apps.district196.org>
2026-01-24 11:48:11 -0600

    Intake subsystem
    Created the Intake subsystem

--- src/main/java/frc/robot26/subsystems/intake/IntakeIO.java
+++ src/main/java/frc/robot26/subsystems/intake/IntakeIO.java
@@ -1,3 +1,27 @@
 package frc.robot26.subsystems.intake;
 
-public class IntakeIO {}
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.Volts;
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Voltage;
+import org.littletonrobotics.junction.AutoLog;
+
+public interface IntakeIO {
+  @AutoLog
+  public static class IntakeIOInputs {
+    public boolean intakeConnected = false;
+    public AngularVelocity intakeVelocity = RadiansPerSecond.of(0.0);
+    public Voltage intakeAppliedVolts = Volts.of(0.0);
+    public Current intakeCurrent = Amps.of(0.0);
+    public Angle intakePosition;
+  }
+
+  /** Updates the set of loggable inputs. */
+  public default void updateInputs(IntakeIOInputs inputs) {}
+
+  public default void setOpenLoop(Voltage output) {}
+}
--- src/main/java/frc/robot26/subsystems/intake/IntakeIOSim.java
+++ src/main/java/frc/robot26/subsystems/intake/IntakeIOSim.java
@@ -1,3 +1,71 @@
 package frc.robot26.subsystems.intake;
 
-public class IntakeIOSim {}
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.Seconds;
+import static edu.wpi.first.units.Units.Volts;
+import static frc.robot26.subsystems.intake.IntakeConstants.GEARING;
+import static frc.robot26.subsystems.intake.IntakeConstants.SUPPLY_CURRENT_LIMIT;
+import static frc.robot26.subsystems.intake.IntakeConstants.Sim;
+
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+import org.ironmaple.simulation.motorsims.MapleMotorSim;
+import org.ironmaple.simulation.motorsims.SimMotorConfigs;
+import org.ironmaple.simulation.motorsims.SimulatedMotorController;
+
+public class IntakeIOSim implements IntakeIO {
+  // TODO: change to correct motor
+  private static final DCMotor intakeGearbox = DCMotor.getKrakenX60(2);
+  private final SimulatedMotorController.GenericMotorController intakeMotorController;
+  private final MapleMotorSim intakeMotor;
+  private boolean isClosedLoop = false;
+  private Voltage intakeAppliedVoltage = Volts.of(0);
+  private final SimpleMotorFeedforward feedForwardController =
+      new SimpleMotorFeedforward(Sim.kS, Sim.kV, Sim.kA);
+
+  private final FlywheelSim intakeSim =
+      new FlywheelSim(
+          LinearSystemId.createFlywheelSystem(intakeGearbox, 0.1, GEARING),
+          intakeGearbox,
+          0.000015);
+
+  public IntakeIOSim() {
+    intakeMotor =
+        new MapleMotorSim(
+            new SimMotorConfigs(intakeGearbox, GEARING, Sim.MOTOR_LOAD_MOI, Sim.FRICTION_VOLTAGE));
+    intakeMotorController =
+        intakeMotor.useSimpleDCMotorController().withCurrentLimit(SUPPLY_CURRENT_LIMIT);
+  }
+
+  @Override
+  public void setOpenLoop(Voltage output) {
+    intakeAppliedVoltage = output;
+    isClosedLoop = false;
+  }
+
+  @Override
+  public void updateInputs(IntakeIOInputs inputs) {
+    var angularVelocity = intakeSim.getAngularVelocityRadPerSec();
+    if (isClosedLoop) {
+      var feedForwardVolts = feedForwardController.calculate(angularVelocity);
+      intakeAppliedVoltage = Volts.of(feedForwardVolts);
+    }
+
+    intakeMotorController.requestVoltage(intakeAppliedVoltage);
+    intakeSim.setInputVoltage(intakeMotor.getAppliedVoltage().in(Volts));
+    intakeMotor.update(Seconds.of(TimedRobot.kDefaultPeriod));
+    intakeSim.update(TimedRobot.kDefaultPeriod);
+
+    // Update motor inputs
+    inputs.intakeConnected = true;
+    inputs.intakeAppliedVolts = intakeAppliedVoltage;
+    inputs.intakeCurrent = Amps.of(intakeSim.getCurrentDrawAmps());
+    inputs.intakeVelocity = AngularVelocity.ofBaseUnits(angularVelocity, RadiansPerSecond);
+  }
+}
