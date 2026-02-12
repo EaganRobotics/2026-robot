bdf753c5fea15faa54bb5f97fcd3f88c82d6c283
Anvith <146397591+AnvithPothula@users.noreply.github.com>
2026-02-11 20:41:34 -0600

    Added intake limit switch (#33)
    * Added intake limit switch
    
    * Copilot suggested fixes
    
    * Deleted print statement
    
    * Update photonlib 20206.2.1 -> 2026.2.2
    
    * Moved DeployState to IntakeIO.java

--- src/main/java/frc/robot26/RobotContainer.java
+++ src/main/java/frc/robot26/RobotContainer.java
@@ -22,7 +22,6 @@ import frc.robot26.subsystems.drive.ModuleIO;
 import frc.robot26.subsystems.drive.ModuleIOSim;
 import frc.robot26.subsystems.drive.ModuleIOTalonFX;
 import frc.robot26.subsystems.intake.Intake;
-import frc.robot26.subsystems.intake.IntakeConstants.DeployState;
 import frc.robot26.subsystems.intake.IntakeIO;
 import frc.robot26.subsystems.intake.IntakeIOSim;
 import frc.robot26.subsystems.intake.IntakeIOTalonFX;
@@ -184,8 +183,8 @@ public class RobotContainer extends frc.lib.infrastructure.RobotContainer {
                 .ignoringDisable(true)
                 .withName("RobotContainer.driverZeroCommand"));
     driverController.b().whileTrue(intake.setOpenLoop(Volts.of(-3)));
-    driverController.x().onTrue(intake.setDeployPosition(DeployState.EXTENDED));
-    driverController.y().onTrue(intake.setDeployPosition(DeployState.RETRACTED));
+    driverController.x().onTrue(intake.setDeployPosition(IntakeIO.DeployState.EXTENDED));
+    driverController.y().onTrue(intake.setDeployPosition(IntakeIO.DeployState.RETRACTED));
   }
 
   @Override
--- src/main/java/frc/robot26/subsystems/intake/Intake.java
+++ src/main/java/frc/robot26/subsystems/intake/Intake.java
@@ -4,23 +4,40 @@ import static edu.wpi.first.units.Units.Volts;
 
 import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot26.subsystems.intake.IntakeConstants.DeployState;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import java.util.function.DoubleSupplier;
 import org.littletonrobotics.junction.Logger;
 
 public class Intake extends SubsystemBase {
   private final IntakeIO io;
   private final IntakeIOInputsAutoLogged inputs = new IntakeIOInputsAutoLogged();
+  public final Trigger limitHit = new Trigger(() -> inputs.limit);
+  private IntakeIO.DeployState currentState = IntakeIO.DeployState.RETRACTED;
 
   public Intake(IntakeIO io) {
     this.io = io;
+
+    limitHit.onTrue(
+        Commands.runOnce(
+                () -> {
+                  currentState = IntakeIO.DeployState.RETRACTED;
+                  io.setDeployOpenLoop(Volts.of(0));
+                  // io.zeroEncoder();
+
+                },
+                this)
+            .withName("Intake.limitHit"));
   }
 
   @Override
   public void periodic() {
     io.updateInputs(inputs);
     Logger.processInputs("Intake", inputs);
+
+    Logger.recordOutput("Intake/CurrentState", currentState.toString());
+    Logger.recordOutput("Intake/LimitHit", inputs.limit);
   }
 
   public Command setOpenLoop(Voltage output) {
@@ -34,11 +51,32 @@ public class Intake extends SubsystemBase {
         .withName("Intake.setOpenLoop");
   }
 
-  public Command setDeployPosition(DeployState state) {
+  public Command setDeployOpenLoop(Voltage output) {
+    return this.startEnd(
+            () -> {
+              io.setDeployOpenLoop(output);
+            },
+            () -> {
+              io.setDeployOpenLoop(Volts.of(0));
+            })
+        .withName("Intake.setDeployOpenLoop");
+  }
+
+  public Command setDeployPosition(IntakeIO.DeployState state) {
     return this.runOnce(
             () -> {
+              currentState = state;
               io.setDeployPosition(state);
             })
+        .andThen(
+            state == IntakeIO.DeployState.RETRACTED
+                ? Commands.waitUntil(limitHit)
+                : Commands.none())
+        .andThen(
+            Commands.runOnce(
+                () -> {
+                  io.setDeployOpenLoop(Volts.of(0));
+                }))
         .withName("Intake.setDeployPosition");
   }
 
