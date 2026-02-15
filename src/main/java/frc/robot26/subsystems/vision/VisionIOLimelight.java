72f3252b5d71b89bee41986e55aa13d486dcefd7
Griffin <krima724@gmail.com>
2026-02-14 18:44:48 -0600

    limeligth stuff

--- src/main/java/frc/robot26/subsystems/vision/VisionIOLimelight.java
+++ src/main/java/frc/robot26/subsystems/vision/VisionIOLimelight.java
@@ -51,6 +51,15 @@ public class VisionIOLimelight implements VisionIO {
         table.getDoubleArrayTopic("botpose_orb_wpiblue").subscribe(new double[] {});
   }
 
+  /*
+   *
+   * 9.898 x
+   * -11.512 y
+   * 8.474 z
+   * 32.2 degrees
+   *
+   */
+
   @Override
   public void updateInputs(VisionIOInputs inputs) {
     // Update connection status based on whether an update has been seen in the last
