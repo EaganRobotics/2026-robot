898964318eb9a66bc23dce58a9403aeecbf9011d
Griffin <krima724@gmail.com>
2026-02-25 20:03:21 -0600

    Drive Auto Settings

--- src/main/java/frc/robot26/subsystems/drive/Drive.java
+++ src/main/java/frc/robot26/subsystems/drive/Drive.java
@@ -45,6 +45,7 @@ import frc.robot26.generated.TunerConstants;
 import frc.robot26.util.LocalADStarAK;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import org.ironmaple.simulation.drivesims.COTS;
 import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
 
@@ -63,7 +64,7 @@ public class Drive extends SubsystemBase {
   // PathPlanner config constants
   private static final double ROBOT_MASS_KG = 74.088;
   private static final double ROBOT_MOI = 6.883;
-  private static final double WHEEL_COF = 1.2;
+  private static final double WHEEL_COF = COTS.WHEELS.SDS_MOLDED_SPIKE_TREAD.cof;
   private static final RobotConfig PP_CONFIG =
       new RobotConfig(
           ROBOT_MASS_KG,
