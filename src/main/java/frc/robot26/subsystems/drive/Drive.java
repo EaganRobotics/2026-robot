509ae2a5f8469b951dbf2d493499d7580cf4cc33
scuyul <104639986+scuyul@users.noreply.github.com>
2026-03-04 18:26:14 +0000

    style: auto-fix formatting with Spotless [skip ci]

--- src/main/java/frc/robot26/subsystems/drive/Drive.java
+++ src/main/java/frc/robot26/subsystems/drive/Drive.java
@@ -45,8 +45,8 @@ import frc.robot26.generated.TunerConstants;
 import frc.robot26.util.LocalADStarAK;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
-import org.ironmaple.simulation.drivesims.COTS;
 import java.util.function.Consumer;
+import org.ironmaple.simulation.drivesims.COTS;
 import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
 
@@ -66,7 +66,7 @@ public class Drive extends SubsystemBase {
   public static final double ROBOT_MASS_KG = 74.088;
   private static final double ROBOT_MOI = 6.883;
   private static final double WHEEL_COF = COTS.WHEELS.SDS_MOLDED_SPIKE_TREAD.cof;
-//   public static final double WHEEL_COF = 1.2;
+  //   public static final double WHEEL_COF = 1.2;
   private static final RobotConfig PP_CONFIG =
       new RobotConfig(
           ROBOT_MASS_KG,
