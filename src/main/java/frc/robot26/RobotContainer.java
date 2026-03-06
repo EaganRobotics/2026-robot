ef2a98222c5d708bd7d74ea506337fefb92d5ce3
ds <ds@team2220.org>
2026-03-06 10:03:15 -0600

    fixed x mode

--- src/main/java/frc/robot26/RobotContainer.java
+++ src/main/java/frc/robot26/RobotContainer.java
@@ -330,7 +330,7 @@ public class RobotContainer extends frc.lib.infrastructure.RobotContainer {
     //     .whileTrue(ShooterCommands.shootManualAim(shooter, floor, feeder, drive));
 
     // driverController.a().whileTrue(feeder.setTunableFeeder());
-    driverController.x().whileTrue(SnapCommands.tuneableFlipitySnipitySnap(drive));
+    // driverController.x().whileTrue(SnapCommands.tuneableFlipitySnipitySnap(drive));
     // driverController.b().whileTrue(shooter.setTunableShooter());
     // driverController.b().whileTrue(floor.setTunableFloor());
     driverController.y().whileTrue(RollerCommands.shootOpenLoop(floor, feeder));
