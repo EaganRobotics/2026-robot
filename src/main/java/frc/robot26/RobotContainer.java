5e0dadaee3bab4a616d7bd41e959d97448df52c1
notGriffin <krima724@gmail.com>
2026-03-29 00:09:34 -0500

    commeneted out bad code

--- src/main/java/frc/robot26/RobotContainer.java
+++ src/main/java/frc/robot26/RobotContainer.java
@@ -58,8 +58,7 @@ import frc.robot26.subsystems.vision.Vision.VisionConsumer;
 import frc.robot26.subsystems.vision.VisionConstants;
 import frc.robot26.subsystems.vision.VisionIOLimelight;
 import frc.robot26.subsystems.vision.VisionIOPhotonVisionSim;
-import frc.robot26.util.Ai.AIBrain;
-import frc.robot26.util.Ai.AIRobotInSimulation;
+// import frc.robot26.util.Ai.AIBrain;
 import org.ironmaple.simulation.SimulatedArena;
 import org.ironmaple.simulation.drivesims.SwerveDriveSimulation;
 import org.ironmaple.simulation.seasonspecific.rebuilt2026.Arena2026Rebuilt;
@@ -493,8 +492,8 @@ public class RobotContainer extends frc.lib.infrastructure.RobotContainer {
   @Override
   public void simulationInit() {
     if (!(SimulatedArena.getInstance() instanceof Arena2026Rebuilt arena)) return;
-    AIRobotInSimulation
-        .startOpponentRobotSimulations(); // If you want to remove the ais comment out this line
+    // AIRobotInSimulation.startOpponentRobotSimulations(); // If you want to remove the ais comment
+    // out this line
     arena.getBlueHub().setOnScoredCallback((gp) -> System.out.println("Blue Hub Scored!"));
     arena.getRedHub().setOnScoredCallback((gp) -> System.out.println("Red Hub Scored!"));
     arena.setNeutralFuelCount(80); // for performance
@@ -504,7 +503,7 @@ public class RobotContainer extends frc.lib.infrastructure.RobotContainer {
   public void simulationPeriodic() {
     if (!(SimulatedArena.getInstance() instanceof Arena2026Rebuilt arena)) return;
 
-    AIBrain.update(drive.getPose());
+    // AIBrain.update(drive.getPose());
     Logger.recordOutput("RobotStorage/StoredBalls", RobotGamePieceStorage.getStoredBalls());
 
     if (shooterIOSim != null) {
