14f5ce35ed08e40486a2fe2e8c0ebbb35357becd
Scuyul <819178@apps.district196.org>
2026-04-24 23:49:29 -0500

    code

--- src/main/java/frc/robot26/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot26/subsystems/shooter/ShooterConstants.java
@@ -13,7 +13,7 @@ import frc.lib.tunables.*;
 
 public class ShooterConstants {
   public static final double joystickSpeedMultiplier = 0.85;
-  public static final Current SUPPLY_CURRENT_LIMIT = Amps.of(50);
+  public static final Current SUPPLY_CURRENT_LIMIT = Amps.of(25); // 50
   public static final Current SUPPLY_CURRENT_LIMIT_HOOD = Amps.of(10);
   public static final Current STATOR_CURRENT_LIMIT_HOOD = Amps.of(15);
   public static final double GEARING_SHOOTER = 30.0 / 18.0;
